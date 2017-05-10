4c1ee9e8bf4f10d323beb534f240d9490ce124fd
datmass <datmass92@gmail.com>
2017-05-10 10:28:34 +0200

    Added MySqlManager to connect to MySqlDatabases

---
+++ Kassensystem/DbManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassensystem
+{
+    public abstract class DbManager
+    {
+        protected string conStr;
+
+        public abstract void Connect();
+
+        public abstract void Close();
+    }
+}

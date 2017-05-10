5997ae810b2616894a4fdc6f497ac4a3505384da
datmass <datmass92@gmail.com>
2017-05-10 10:22:46 +0200

    Added initialdata for project

---
+++ Kassensystem/Frm_Kassensystem.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Kassensystem
+{
+    partial class Frm_Kassensystem
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_Kassensystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Frm_Kassensystem";
+            this.Text = "Emmas Kasse";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+

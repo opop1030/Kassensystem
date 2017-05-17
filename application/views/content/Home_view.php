346ffd2a5b7400ad8b2c79642f8e9a6103487dbb
datmass <datmass92@gmail.com>
2017-05-17 14:08:18 +0200

    entfernen des autoloads des Datenbankmoduls, da noch keine datenbank besteht um fehler zu vermeiden

--- application/views/content/Home_view.php
+++ application/views/content/Home_view.php
@@ -1,9 +1,9 @@
 <div>
     <h1>Willkommen!</h1>
     <hr/>
-    <p>Hier entsteht das Kassensystem Emma!<br/>Das Produkt ist ab dem 07.06.2017 verfügbar!</p>
+    <p>Hier entsteht das Kassensystem Emma!<br/>Das Produkt ist ab dem 07.06.2017 verf&uuml;gbar!</p>
     <br/>
     <?php
-        echo "";
+        echo date('d.m.Y');
     ?>
 </div>
--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,6 +1,6 @@
 <br/>
 <footer>
-    <p>&copy;2017 by Andreas Horstmann</p>
+    <p>&copy;2017 by Andreas Horstmann, Jannis Derksen, Murat Tahta, Martin Moors</p>
     <br/>
     <?php echo anchor("impressum", "Impressum") ?>
 </footer>

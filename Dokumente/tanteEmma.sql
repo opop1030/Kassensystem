827dac0d2bb5da19776f19359208e63a070490d3
muratdev <muratmahir.t@gmail.com>
2017-05-24 11:03:22 +0200

    SQL-Skript aktualisiert: Regeln für referentielle Integrität hinzugefügt.

--- Dokumente/tanteEmma.sql
+++ Dokumente/tanteEmma.sql
@@ -19,29 +19,29 @@ CREATE TABLE person (
     person_ort varchar(40) not null,
     person_tel numeric(38) not null,
     person_email varchar(50) not null
-);
+) ENGINE=INNODB;
     
 CREATE TABLE angestellte (
 	ang_id int,
 	ang_gehalt numeric(9,2) DEFAULT 0
-);
+) ENGINE=INNODB;
 
 CREATE TABLE kunde (
 	kunde_id int,
     kunde_umsatz numeric(9,2) DEFAULT 0
-);
+) ENGINE=INNODB;
 
 CREATE TABLE artikel (
-	art_id int not null auto_increment,
+	art_id int not null primary key auto_increment ,
     art_name varchar (100) not null,
     art_kategorie varchar(50) not null,
     art_bestand int default 0,
     art_preis numeric(9,2) not null
-);
+) ENGINE=INNODB;
 
 CREATE TABLE art_kategorie (
 	art_kat_name varchar(40) not null primary key
-);
+) ENGINE=INNODB;
     
 CREATE TABLE bestellung (
 	bestell_id	int not null primary key auto_increment,
@@ -49,29 +49,53 @@ CREATE TABLE bestellung (
     ang_id int not null,
     datum date not null,
     zeit timestamp not null
-);
+    )ENGINE=INNODB;
 
 CREATE TABLE warenkorb (
 	wk_id int not null primary key auto_increment,
     wk_bestell_id int,
     wk_artikel_name varchar(100)
-);
+)ENGINE=INNODB;
     	
 ALTER TABLE angestellte
 	ADD CONSTRAINT ang_id_fk FOREIGN KEY(ang_id)
-    REFERENCES person(person_id);
+    REFERENCES person(person_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
 
 ALTER TABLE kunde
 	ADD CONSTRAINT kunde_id_fk FOREIGN KEY(kunde_id)
-    REFERENCES person(person_id);
+    REFERENCES person(person_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
 
 ALTER TABLE artikel 
 	ADD CONSTRAINT art_kat_fk FOREIGN KEY(art_kategorie)
 	REFERENCES art_kategorie(art_kat_name)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE;
     
-ALTER TABLE warenkorb (
-	ADD CONSTRAINT wk_
+ALTER TABLE warenkorb 
+	ADD CONSTRAINT wk_best_fk FOREIGN KEY (wk_bestell_id)
+    REFERENCES bestellung(bestell_id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT;
+    
+ALTER TABLE bestellung
+	ADD CONSTRAINT best_kunde_fk FOREIGN KEY (kunde_id)
+    REFERENCES kunde(kunde_id)
+	ON UPDATE CASCADE
+    ON DELETE RESTRICT;
+    
+ALTER TABLE bestellung
+	ADD CONSTRAINT best_ang_fk FOREIGN KEY (ang_id)
+    REFERENCES angestellte(ang_id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT;
+    
 
+    
+    
 
 
 

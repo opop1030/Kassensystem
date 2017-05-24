606516d4e239af5104dfc54fef955ce5f72fa7a8
datmass <datmass92@gmail.com>
2017-05-24 10:19:27 +0200

    added bootstrap suppor

--- application/views/includes/navi.php
+++ application/views/includes/navi.php
@@ -1,17 +1,18 @@
-<div class="w3-container">
-    <ul class="w3-navbar">
-        <?php
-            //$status wird übergeben und ist ein array... status[navi] ist die sichtbarkeit des Navis und
-            //bereits behandelt in content.php
-            echo anchor('home', 'Home');
-            if ($status["login"] === true)
-            {
-                echo anchor('profile', 'Profil');
-                echo anchor('logout', 'Logout');
-            }
-            else{
-                echo anchor('login', 'Login', 'class="w3-hover-blue-grey"');
-            }
-        ?>
-    </ul>
+<div class="col-sm-3 nopadding menu">
+    <div class="menupoint">
+        <a> Kasse</a>
+    </div>
+    <div class="menupoint">
+        <a>Vorbestellen</a>
+    </div>
+    <div class="menupoint">
+        <a>Lager</a>
+    </div>
+    <div class="menupoint">
+        <a>Kunden Verwalten</a>
+    </div>
+    <!-- Kann nur der Admin -->
+    <div class="menupoint">
+        <a>Mitarbeiter Verwalten</a>
+    </div>
 </div>

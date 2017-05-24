56451da763c286d0b6656fb8663c1063d215e7df
datmass <datmass92@gmail.com>
2017-05-24 12:54:26 +0200

    added views

---
+++ application/views/content/Datahandler_View.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andy
+ * Date: 24.05.2017
+ * Time: 12:49
+ */
---
+++ application/views/content/Logout_View.php
@@ -0,0 +1,5 @@
+<h1>Sie wurden abgemeldet!</h1>
+<hr/>
+<p>Sie wurden abgemeldet! Bitte zum Nutzen der Kasse einloggen!</p>
+<br/>
+<?php echo anchor('login', 'Hier einloggen!') ?>

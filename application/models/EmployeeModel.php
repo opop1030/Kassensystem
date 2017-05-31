6efb2d4520e13b70463c594d03cdd01024f78768
datmass <datmass92@gmail.com>
2017-05-31 12:56:57 +0200

    einfuegen von Loginmachanismen und vorbereitung fuer das login

--- application/models/EmployeeModel.php
+++ application/models/EmployeeModel.php
@@ -2,7 +2,23 @@
 
 class EmployeeModel extends CI_Model{
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
+    }
+
+    public function verify($name, $pwd)
+    {
+        $q = $this
+            ->db
+            ->where("username", $id)
+            ->where("pwd", sha1($pwd))
+            ->limit(1)
+            ->get("member");
+        if ($q->result() != null) {
+            return $q->row(0);
+        } else {
+            return false;
+        }
     }
 }
